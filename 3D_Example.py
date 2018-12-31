b641e3614f32ebe24b1f3bcc100711ba016b607d
Tyler Martin <tyler@thompsonhouston.com>
2018-12-30 18:30:40 -0600

    Update 3D_Example.py
    Added some comments in the code

--- 3D_Example.py
+++ 3D_Example.py
@@ -12,15 +12,19 @@ km = KMeans(n_clusters=3)
 y_means = km.fit_predict(X)
 labels = km.labels_
 
-plt.plot(y_means)
-
 # Visualizing the Clusters
 plt.scatter(X[y_means == 0, 0], X[y_means == 0, 1], s = 100, c = 'red', label = 'Group 1')
 plt.scatter(X[y_means == 1, 0], X[y_means == 1, 1], s = 100, c = 'blue', label = 'Group 2')
 plt.scatter(X[y_means == 2, 0], X[y_means == 2, 1], s = 100, c = 'green', label = 'Group 3')
+''' This is the normal way to do it. Regular ole 2D Scatter Plotting.'''
 
+# Visualizing the Clusters, with EdgeColor on: 
+plt.scatter(X[y_means == 0, 0], X[y_means == 0, 1], s = 100, c = 'red', edgecolor = 'blue', label = 'Group 1')
+plt.scatter(X[y_means == 1, 0], X[y_means == 1, 1], s = 100, c = 'blue', edgecolor = 'slate', label = 'Group 2')
+plt.scatter(X[y_means == 2, 0], X[y_means == 2, 1], s = 100, c = 'green', edge color = 'black', label = 'Group 3')
+''' The edge color makes the 2D graph much more visually appealing.'''
 
-#Plotting 3D
+#Plotting 3D - The sweet new way I found to represent data. (K-Means is applied to the 3D space)
 fig = plt.figure(1, figsize=(7,7))
 ax = Axes3D(fig, rect=[0, 0, 0.95, 1])
 ax.scatter(X[:, 3], X[:, 0], X[:, 2],
